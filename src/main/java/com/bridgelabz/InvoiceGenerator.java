99d04e0ff37e589e0f29de4696dd35779016d874
Rohit Patro <rohitpatro6@gmail.com>
2021-08-13 17:27:51 +0530

    ADDED:Total Fare For The Journey Added

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab Invoice Generator");
+    private final double COST_PER_KILOMETER = 10;
+    private final int COST_PER_MINUTE = 1;
+
+    public double calculateFare(double distance, int time) {
+        return (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+
     }
+
 }
