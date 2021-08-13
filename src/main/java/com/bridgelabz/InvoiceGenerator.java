7b99ec0e3a25ea085d3174aff09ba1943e59c1f3
Rohit Patro <rohitpatro6@gmail.com>
2021-08-13 17:32:21 +0530

    MODIFIED:Code for UC1 Modified

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,12 +1,15 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    private final double COST_PER_KILOMETER = 10;
-    private final int COST_PER_MINUTE = 1;
+    private static final double MIN_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
-        return (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
-
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 
 }
