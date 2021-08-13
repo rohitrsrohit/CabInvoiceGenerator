90a73a1e02db95b4affe45d12895370fbeebbfc9
Rohit Patro <rohitpatro6@gmail.com>
2021-08-13 17:45:31 +0530

    ADDED:Multiple Rides Added

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    static InvoiceGenerator invoiceGenerator;
+
+    @BeforeClass
+    public static void Obj(){
+        invoiceGenerator = new InvoiceGenerator();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
@@ -19,5 +26,12 @@ public class InvoiceGeneratorTest {
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalfare() {
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1) };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
